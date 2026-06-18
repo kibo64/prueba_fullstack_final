2c318f2552044d2a2250368c4e6a7bec2875d20a
kibo64 <ni.chauca@duocuc.cl>
2026-06-18 17:04:14 -0400

    rename folders for railway

---
+++ ms-asistencia/src/main/java/com/example/ms_asistencia/client/CursoClient.java
@@ -0,0 +1,31 @@
+package com.example.ms_asistencia.client;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.example.ms_asistencia.dto.ApiResponse;
+import com.example.ms_asistencia.dto.CursoResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CursoClient {
+
+    private final WebClient webClient;
+
+    private final String BASE_URL = "http://ms-cursos:8085/api/v1/cursos/";
+
+    public CursoResponse obtenerCurso(Long id, String token) {
+
+        ApiResponse<CursoResponse> response = webClient.get()
+                .uri(BASE_URL + id)
+                .header("Authorization", token)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<ApiResponse<CursoResponse>>() {})
+                .block();
+
+        return response != null ? response.getData() : null;
+    }
+}
---
+++ ms-profesores/src/main/java/com/example/ms_profesores/client/FacultadClient.java
@@ -0,0 +1,31 @@
+package com.example.ms_profesores.client;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.example.ms_profesores.dto.ApiResponse;
+import com.example.ms_profesores.dto.FacultadResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class FacultadClient {
+
+    private final WebClient webClient;
+
+    private final String BASE_URL = "http://ms-facultad:8082/api/v1/facultades/";
+
+    public FacultadResponse obtenerFacultad(Long id, String token) {
+
+        ApiResponse<FacultadResponse> response = webClient.get()
+                .uri(BASE_URL + id)
+                .header("Authorization", token)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<ApiResponse<FacultadResponse>>() {})
+                .block();
+
+        return response != null ? response.getData() : null;
+    }
+}
